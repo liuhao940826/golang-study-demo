ae7cab540d679ce52aa74807755c06976a8a09a8
liuhao <alan.liu@yiako.com>
2019-09-09 10:05:16 +0800

    fix

--- main/结构体方法值传递和引用传递.go
+++ main/结构体方法值传递和引用传递.go
@@ -7,26 +7,25 @@ type Test struct {
 	B string
 }
 
-func main(){
+func main() {
 	// 引用类型
-	map_param := make(map[int64]string,0)
+	map_param := make(map[int64]string, 0)
 	fmt.Println(map_param)
 	mapfunc(map_param)
 	fmt.Println(map_param)
 
-	slice_param := make([]int64,0)
-	slice_param = append(slice_param,999)
+	slice_param := make([]int64, 0)
+	slice_param = append(slice_param, 999)
 	fmt.Println(slice_param)
 	slicefunc(slice_param)
 	fmt.Println(slice_param)
 
-
 	// 值类型
-	arr_param := [5]int64{1,2,3,4}
+	arr_param := [5]int64{1, 2, 3, 4}
 	fmt.Println(arr_param) // 1,2,3,4
 	arrfunc(arr_param)
 	fmt.Println(arr_param) // 1,2,3,4
-	test := Test{A:1,B:"ooo",}
+	test := Test{A: 1, B: "ooo"}
 	fmt.Println(test) // 1,ooo
 	testfunc(test)
 	fmt.Println(test) // 1,ooo
@@ -34,19 +33,19 @@ func main(){
 
 func mapfunc(strings map[int64]string) {
 
-	strings[]
+	strings[1] = "哈哈哈"
 
 }
 
-func slicefunc(param []int64){
-	param[0]=10000
+func slicefunc(param []int64) {
+	param[0] = 10000
 }
 
-func testfunc(a Test){
-	a.A=9999
-	a.B="hah"
+func testfunc(a Test) {
+	a.A = 9999
+	a.B = "hah"
 }
 
-func arrfunc(param [5]int64){
-	param[0]=10000
+func arrfunc(param [5]int64) {
+	param[0] = 10000
 }
